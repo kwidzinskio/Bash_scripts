98f39291e62ecaabb6e9e1017d8d6d8a9cb4e392
Oskar Kwidziński <kwidzinskio@gmail.com>
2022-11-03 20:04:45 +0100

    change username definition

--- create_user_0.2.0.sh
+++ create_user_0.2.0.sh
@@ -44,12 +44,12 @@ SCRIPT_VERSION="0.2.0"
 # employees
 USERNAME_OSKAR="oskarkwidzinski"
 PUB_KEY_OSKAR="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCycSz7Ey7qnI1AyoDu+JpAc9Gvb9q/oLexacw0UwDjhlm1Cho5llkFZNenUZrx6e3JqesTa6ZwE95PebBsCPOqTHgvHsk9l2/6fBZZ/F2sx/qn45zOAGASfU/YTbbjyoA1gfT2fmu9hTgOaXSfuStmDyIzcgWtMmbWSN4z8zt0T3mQnZRwz6KzCYe0k8drtS2b3Vn1mHGq69lwcFngWdOjQGwBoRnV1sWG7eL7KA+iwEQGwQSDD9S6bio517sfSFV7cmah0z8uuQdhirp3sarqKfmx/QPyJYwL/82FGhrqfwAeHkC5WJY0W2QH1Zsojl8kOipXlFzXNz9EBiqQcw9sKEtTLspGq/Hx+zQabagSE6HQ/R4gk+Ibz2CELHU0LcqRpyHm9Gf1PEnE0X52VSK4CWk8n55C06ehC4FZaBXIXTp0xQaGf0aMRFeAe6Vty/Gsr0JGWAldmrfDViaKI2HLtnuxIeBpy1fT4AtfmUHkn/oqseyC7V9MMdUHsFMZ1+k= kwidz@DESKTOP-90DFH4J"
-USERNAME_MICHAL="michalandrzejewski"
-PUB_KEY_MICHAL="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTignBLADbdJxX34+l/yy3M5iJLmNV54YSn6mtg4Lc9/2HIavPwO6afZXBYFD5YqVBM+OVvLHGuiRFeIA5mNOSjAhqCxFcikm3aFrE+ceniTp1hfQByJeSA6GvoAXYPIu+977np794CVfkMXNfD5zfsRyPfj8Os3nRyc0a/OBD9kK7s5LXLbcFFftOpkevXwmp5b5jaFOIWqFONz0RUp+tfRsW8CKiF+f+Ixdi7E1xJXaqzBUkjXbrRSqERvlad65muY67hKnTeq2ru3MrmajL5pE0EzrsiBfnkCL5dXsRQiv0cWUfKxnSpKUg7HQpjefyvt1Z0pOZkoAkPQuSEqcsfz38A+hKHLawzfFaPO3n8GOAidr4IbpMlBZw2/aXXYhH+CMW3n3lKRYfq7XQuSu5rMPpYUGXNRq81Os+SS1Xhmgl41HB5+2WSY7OFR/DP9Izxf3ihwgseSpAKmax6KcMYhjZfytOFwh8CVGr9T2nh4fGF7Rcq/6iFxOzRhszZvk= michal@michal-VirtualBox"
-USERNAME_DAWID="dawidnowalinski"
-PUB_KEY_DAWID="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDqrMF8mXda37k2bNB3iLrzjt7c/3Mmq6LFYAwvQbHEVmiIdhksmBAyWGQudnZSOsi/PgS6+iZS/eVnbr+3eBs/H4py6eB5LCME0PkHICHC8kIx8nygiIlfgCshvVCDuuzaFoNk3RG0soORKwZ6o2cMogvhkifk0U4omEbzulFucqeAOFE2yOfSf91UH3uhBKqN6+pdaw18O8r9tD0plEeN7tP9foff8BOAX9PhWedopKxoixTdW4TTcTNlo/CiWrFB8LxHq0Mfb8kJtt5lBiZSl6y/gHsQB38wdyLQaxUJYftD7HGoHUTZ3AyAV40bQfFm3URuh3On4fcdpe/MWfjGFA3auDJwihHorI69OGxsaKlbdDOiE8wTqOJCBDDRn1E3814D4ePE1SzMcV73qQ9HXhVRGJpUpkwZ4UxcN0cWNBdvFzjJPxHP9WoSwHSX9zcub98T+5XTdLa36OEXSyJ1FlXdj8aU7VgjcbK1DaN1g5de5soYgGqextOWUzA+SuAV4ya3eYJLK1cnDsTn3kYKzF40XqMlBNP3Lsznsy0w1L1l41wGLL2WLrPpsN8nJ80tNFb8HWIfYoBqQXMC5iS47+IUkIeymJfsBbukJhh0zldGoeovSjvxz/74G7o097Wk98nuEiwAavtWH4ZqVkHMuBqp927E5GNNCXz8L0X0mw== knifcio@KnifcioXPS"
-USERNAME_PATRYCJA="patrycjabolius"
-PUB_KEY_PATRYCJA="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNLVbmExtbrbwr3XKNtUiXcqJbzNPcOU/bRgiy8R8Cs0RDFZZqu11ikmSta9ivoEmFHTZ9o+CJsRkzZWe1GlSHd8/uJDT/Mc5hK5yxIrhJ5++N/RLi9roHdnKOs+/wHbvryJROGx29WIVqN+wjM6TajH7kSGfCmYTjpDFmKNNPWP8grautjhAZlUxCRhy8KHzO2DUYDX+I78lWagAaksXnFnK2VGINWpav1FQuyY+L6FiCeQK6+gOWFr/eyFEuSwarZm3BoVmLV5QX7yWuV5mWE9kQ9tPh+/2kOavpWdRLC9CQHglEXRyTrCBa2MxCuHsW5LKnW+UYVtO1f+IOLMPsbwQdr0X1qH1lzWOO064yEkIzw8yMxb/qDepYjZ0KyHljNDlhbuoPxNqlxFPIw17fDWGd1BRjhau+w49Hcgug1W1ArkVKrfdEbllDVnFrQ8j5vfm4cGMcss/64r9v/k4WjnkOcRgaeeamD9U9tYl5EV5ouDkrXud0C5Chm+zVZp8= pati@MacBook-Pro-Patrycja.local"
+USERNAME_USER2=""
+PUB_KEY_USER2=""
+USERNAME_USER3=""
+PUB_KEY_USER3=""
+USERNAME_USER4=""
+PUB_KEY_USER4=""
 
 UPDATE="y"
 USER_OPTION=1
@@ -205,9 +205,9 @@ while [ $USER_OPTION != q ] && [ $USER_OPTION != Q ]; do
     echo "Choose operation:"
     echo "1 - create new user"
     echo "2 - create user '$USERNAME_OSKAR'"
-    echo "3 - create user '$USERNAME_DAWID'"
-    echo "4 - create user '$USERNAME_PATRYCJA'"
-    echo "5 - create user '$USERNAME_MICHAL'"
+    echo "3 - create user '$USERNAME_USER2'"
+    echo "4 - create user '$USERNAME_USER3'"
+    echo "5 - create user '$USERNAME_USER4'"
     echo "9 - list all users"
     echo "0 - delete chosen user"
     echo -e "q - cancel\n"
@@ -247,7 +247,7 @@ while [ $USER_OPTION != q ] && [ $USER_OPTION != Q ]; do
     elif [ $USER_OPTION == 3 ]; then
 
 	if [ $USER == "root" ]; then
-	    create_user $USERNAME_DAWID "$PUB_KEY_DAWID"
+	    create_user $USERNAME_USER2 "$PUB_KEY_USER2"
         else
 	    echo -e "\n*** Operation restricted for root user ***"
         fi
@@ -255,7 +255,7 @@ while [ $USER_OPTION != q ] && [ $USER_OPTION != Q ]; do
     elif [ $USER_OPTION == 4 ]; then
 
 	if [ $USER == "root" ]; then
-	    create_user $USERNAME_PATRYCJA "$PUB_KEY_PATRYCJA"
+	    create_user $USERNAME_USER3 "$PUB_KEY_USER3"
         else
 	    echo -e "\n*** Operation restricted for root user ***"
         fi
@@ -263,7 +263,7 @@ while [ $USER_OPTION != q ] && [ $USER_OPTION != Q ]; do
     elif [ $USER_OPTION == 5 ]; then
 
 	if [ $USER == "root" ]; then
-	    create_user $USERNAME_MICHAL "$PUB_KEY_MICHAL"
+	    create_user $USERNAME_USER4 "$PUB_KEY_USER4"
         else
 	    echo -e "\n*** Operation restricted for root user ***"
         fi
